9264af69575ad9f1fa75c51bdfc4bacab20d3ec5
Nermina Logo Lendo <nerminalogolendo@gmail.com>
2023-04-24 16:03:07 +0200

    c

--- R/script_clasificacion_14042023.R
+++ R/script_clasificacion_14042023.R
@@ -1,32 +1,23 @@
-# Load required libraries
-library(dplyr)
-library(mice)
-library(caret)
-library(themis)
+# Load necessary packages
+
+library(dplyr)      # Provides a set of functions for data manipulation and transformation
+library(mice)       # Provides multiple imputation for missing data
+library(caret)      # Provides a set of functions for training and evaluating predictive models
+library(themis)     # Provides functions for dealing with imbalanced data
+library(randomForest)  # Provides functions for building random forests, an ensemble learning method for classification, regression and other tasks
+library(ggplot2)    # Provides a set of functions for data visualization
+library(mltools)    # Provides a set of functions for machine learning tasks
+library(ggtext)     # Provides a set of functions for using formatted text in ggplot2 graphics
 library(pROC)
-library(randomForest)
-library(ggplot2)
-library(mltools)
-library(ggplot2)
-library(ggtext)
 
-
-
-library(DMwR)
-library(plyr)
-
-
-##########################LOAD DDBB############################################
+# Set working directory and read in data from CSV file
 setwd("C:/Users/Nera/Desktop/TFM_MUBBC_UAM")
 df <- read.csv("BBDD_ASC_CLASIFICACION.csv", sep = ";", header = TRUE)
 
-
-#Cambio a numerico para que limpie cualquier texto que hay en la BBDD
+# Convert columns 2-9 to numeric data type 
 df <- df%>%mutate_at(vars(2:9), as.numeric)
 
-
-
-
+# Recode factor variables for easier interpretation
 df <- df %>%
   mutate(Exome = recode_factor(Exome, "0" = "Negative", "1" = "Positive")) %>%
   mutate(Gender = recode_factor(Gender, "1" = "Male", "2" = "Female")) %>%
@@ -35,77 +26,100 @@ df <- df %>%
   mutate(Delivery_problems = recode_factor(Delivery_problems, "0" = "No", "1" = "Yes")) %>%
   mutate(Age_symptoms = recode_factor(Age_symptoms, "0" = "<1", "1" = "1-3", "2" = ">3"))
 
-
-#Comprobamos la división "equitativa" entre los pacientes con y sin resultados
-#positivos y negativos del exoma
-table(df$Exome)
-
-#We remove the identifiers 
+# Remove rows with missing values in specific columns
 df_no_ID <- df[,c(2:10)]
-
-
-##########################PREPROCESS DDBB#####################################
-
-#Eliminamos las filas de las variables que no vamos a imputar, como la edad
-#de los padres
 df_no_ID <- df_no_ID[complete.cases(df_no_ID[, c("Age_F_Birth", "Age_symptoms")]), ]
 
 
-#Comprobamos el porcentaje de valores perdidos de las columnas que vamos a imputar:
+# Create a frequency table and plot a bar chart of the poportion of Exome values
+table(df_no_ID$Exome)
+my_colors <- c("#600000", "#004400")
+freq_table <- table(df_no_ID$Exome)
+barplot(freq_table, main = "Proportion", xlab = "Exome", ylab = "Frequency", col = my_colors)
+
+# Calculate rows with missing values in specific columns
 mean(is.na(df_no_ID$Age_walking)) * 100
 mean(is.na(df_no_ID$Regression)) * 100
 mean(is.na(df_no_ID$Epilepsy)) * 100
 
-#imputamos los valores perdidos (no tendremos en cuenta que una de las variables
-#tiene más del 18% de NAs)
-mice_object <- mice(df_no_ID[,-9], m = 10, maxit = 100, meth = c("logreg",
-          "pmm", "pmm", "polyreg", "pmm", "logreg", "logreg", "polyreg"))
+# Run multiple imputation on the data using the mice package
+mice_object <- mice(df_no_ID[,-9], m = 5, maxit = 50, method = c("", "", "", "polyreg", "pmm", "logreg", "", ""))
 
+# Retrieve the imputed data from the mice object
 imputed_data <- complete(mice_object)
 
+# Add the Exome column back to the imputed data
 imputed_data <- cbind(imputed_data, df_no_ID$Exome)
 
+# Rename the Exome column to match the original dataset
 imputed_data <- dplyr::rename(imputed_data, "Exome" = "df_no_ID$Exome")
-##########################BALANCE DDBB######################################
-# Set seed for reproducibility
+
+# Create a histogram of the "Age_walking" column in the original data
+hist(df_no_ID$Age_walking, main = "Distribution before imputing", xlab = "Age_walking")
+
+#Create a histogram of the "Age_walking" column in the imputed data
+hist(imputed_data$Age_walking, main = "Distribution after imputing", xlab = "Age_walking")
+
+# Sets the seed for reproducibility
 set.seed(677)
 
-# Create a training-validation partition
+# Create training and testing partitions to evaluate the balancing models
 trainIndex_BALANCE <- createDataPartition(imputed_data$Exome, p = 0.7, list = FALSE)
 train_BD <- imputed_data[trainIndex_BALANCE, ]
 test_BD <- imputed_data[-trainIndex_BALANCE, ]
 
-# Define the control object for train function
+# Set up the train control parameters
 ctrl <- trainControl(method = "cv", number = 5, summaryFunction = twoClassSummary, classProbs = TRUE)
 
-# Define a list of sampling methods
+# Define the sampling methods to use
 sampling_methods <- c("none", "under", "over", "smotenc")
 
-# Train random forest models using different balance methods
+# Create an empty list to store the results
 results <- list()
+
+# Loop through each sampling method and fit a random forest model
 for (method in sampling_methods) {
+  
+  # Use the "none" method as the baseline (no method)
   if (method == "none") {
     model <- caret::train(Exome ~ ., data = train_BD, method = "rf", trControl = ctrl, importance = TRUE)
-  } else if (method == "down") {
-    train_balanced <- caret::downSample(x = train_BD[, -9], y = train_BD$Exome)
-    train_balanced$Exome <- as.factor(train_balanced$Class)
+  } 
+  # Use the undersampling method to balance the classes
+  else if (method == "down") {
+    train_balanced <- caret::downSample(x = train_BD[, -9], y = train_BD$Exome, yname = "Exome")
     model <- caret::train(Exome ~ ., data = train_balanced, method = "rf", trControl = ctrl, importance = TRUE)
-  } else if (method == "over") {
+  } 
+  # Use the oversampling method to balance the classes
+  else if (method == "over") {
     train_balanced <- caret::upSample(x = train_BD[, -9], y = train_BD$Exome, yname = "Exome")
     model <- caret::train(Exome ~ ., data = train_balanced, method = "rf", trControl = ctrl, importance = TRUE)
-  } else if (method == "smotenc") {
+  } 
+  # Use the SMOTE-NC method to balance the classes 
+  else if (method == "smotenc") {
     train_balanced <- smotenc(train_BD, var = "Exome", k = 5, over_ratio = 1)
     model <- caret::train(Exome ~ ., data = train_balanced, method = "rf", trControl = ctrl, importance = TRUE)
   }
+  
+  # Store the results in the list
   results[[method]] <- model
 }
 
-# Evaluate models on the validation set
+# Create an empty list to store the evaluation results for each sampling method
 evaluations <- list()
+
+# For each sampling method, evaluate the model performance on the test data
 for (method in sampling_methods) {
+  
+  # Get the model trained using the current sampling method
   model <- results[[method]]
+  
+  # Generate predicted probabilites for the test data
   pred_prob <- predict(model, newdata = test_BD, type = "prob")[, 2]
+  
+  # Convertt predicted probabilites to predicted class levels (Positive or Negative)
   pred_class <- ifelse(pred_prob > 0.5, "Positive", "Negative")
+  
+  # Store the evaluation metrics for the current sampling method in a dataframe
   evaluations[[method]] <- data.frame(
     Method = method,
     ROC = roc(test_BD$Exome, pred_prob)$auc,
@@ -116,116 +130,102 @@ for (method in sampling_methods) {
   )
 }
 
-
-# Combine evaluations into a single data frame
+# Combine the evaluation results for all sampling methods into a single data frame
 evaluations_df <- do.call(rbind, evaluations)
+
+# Identify the best sampling method based on the average performance across all 
+# evaluation metrics
 best_method <- evaluations_df[which.max(rowMeans(evaluations_df[, -1])), "Method"]
 
-# Print evaluation results
-#print(evaluations_df)
-
-
-#Plot the results
+# Reshape the evaluation data frame into a long formato for plotting
 evaluations_long <- reshape2::melt(evaluations, id.vars = "Method")
 
+# Create a grouped var plot of the evaluation metrics for each sampling method
 ggplot(evaluations_long, aes(x = Method, y = value, fill = variable)) +
-       geom_bar(stat = "identity", position = "dodge") +
-       xlab("Sampling Method") +
-       ylab("Value") +
-       ggtitle("Comparison of Sampling Methods") +
-       scale_fill_discrete(name = "Evaluation Metric")
+  geom_bar(stat = "identity", position = "dodge") +
+  xlab("Sampling Method") +
+  ylab("Value") +
+  ggtitle("Comparison of Sampling Methods") +
+  scale_fill_discrete(name = "Evaluation Metric") 
 
-
-
-# Create an empty plot
-#plot(NULL, xlim=c(0,1), ylim=c(0,1), xlab="False Positive Rate", ylab="True Positive Rate", main="Receiver Operating Characteristic (ROC) Curve")
-
-# Add curves for each sampling method
-#for (method in sampling_methods) {
-#  model <- results[[method]]
-#  pred_prob <- predict(model, newdata = test_BD, type = "prob")[, 2]
-#  roc_obj <- roc(test_BD$exoma, pred_prob, plot=TRUE, col=ifelse(method=="none", "black", "red"))
-#  auc <- round(auc(roc_obj), 3)
-#  legend("bottomright", paste(method, " (AUC=", auc, ")", sep=""), col=ifelse(method=="none", "black", "red"), lty=1)
-#}
-
-
+# Select the sampling method with the best performance
 if (best_method == "over") {
-  # Apply oversampling to balance the data
+  
+  # Upsample the minority class using caret's upSample function
   balanced_df <- caret::upSample(x = imputed_data[, -9], y = imputed_data$Exome, yname = "Exome")
 } else if (best_method == "under") {
-  # Apply undersampling to balance the data
+  # Downsample the majority class using caret's downSample function
   balanced_df <- caret::downSample(x = imputed_data[, -9], y = imputed_data$Exome)
 } else if (best_method == "smotenc") {
-  # Apply SMOTENC to balance the data
+  # Use SMOTENC algorithm to synthetically oversample the minority class
   balanced_df <- smotenc(imputed_data, var = "Exome", k = 5, over_ratio = 1)
 } else if (best_method == "none") {
-  # No balancing method specified
+  # If the best method is "none", use the original imputed data without any sampling
   balanced_df <- imputed_data
 }
 
-
-##########################RANDOM FOREST APLICATION##############################
-
+# Split the balanced data into training and test sets for the RF algorithm
 trainIndex_RF <- createDataPartition(balanced_df$Exome, p = 0.7, list = FALSE)
 train_RF <- balanced_df[trainIndex_RF, ]
 test_RF <- balanced_df[-trainIndex_RF, ]
 
+# Set the number of folds for cross-validation
 k = 10
-# Create an empty vector to store the accuracies for each fold
+
+# Initialize an array to store the accuracy scores for each fold of cross-validation
 cv_accuracies <- rep(0, k)
 
-# Create the folds using the createFolds function from the caret package
+# Create k folds for cross-validation
 folds <- createFolds(train_RF$Exome, k = k)
 
-# Perform k-fold cross-validation
+# Loop over each fold of cross-validation
 for (i in 1:k) {
   
-  # Split the data into train and validation sets for this fold
+  # Split hte data intro training and validation sets for this fold
   train_indices <- unlist(folds[-i])
   valid_indices <- folds[[i]]
   train_cv <- train_RF[train_indices, ]
   valid_cv <- train_RF[valid_indices, ]
   
-  # Train random forest model on the train_cv data
+  # Train random forest model on the training set
   rf_model <- randomForest(Exome ~ ., data = train_cv, importance = TRUE, proximity = TRUE, ntree = 1000)
   
-  # Make predictions on the validation set
+  # Make predictions on the validation set using the trained model
   rf_pred <- predict(rf_model, newdata = valid_cv)
   
-  # Calculate accuracy for this fold and store it in the cv_accuracies vector
+  # Calculate the confusion matrix and accuracy score for this fold
   confusion_matrix <- table(valid_cv$Exome, rf_pred)
   accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
   cv_accuracies[i] <- accuracy
 }
 
-# Calculate the mean and standard deviation of the accuracies over all folds
+# Calculate the mean and standard deviation of the accuracy scores over all folds 
+# of cross-validation
 mean_accuracy <- mean(cv_accuracies)
 sd_accuracy <- sd(cv_accuracies)
 
-# Train the final random forest model on the full train_RF data
+# Train a random forest model
 rf_model <- randomForest(Exome ~ ., data = train_RF, importance = TRUE, 
                          proximity = TRUE, ntree = 1000, mtry = 5, maxnodes = 7)
 
-# Make predictions on the test set
+# Make predictions on the test set using the trained model
 rf_pred <- predict(rf_model, newdata = test_RF)
 
-# Print the confusion matrix, accuracy, mean accuracy, and standard deviation
+# Calculate the confusion matrix and accuracy of the predictions
 confusion_matrix <- table(test_RF$Exome, rf_pred)
 accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
+
+# Print the confusion matrix and accuracy on the test set
 cat("Confusion Matrix:\n")
 print(confusion_matrix)
 cat("\nAccuracy on Test Set:", accuracy)
+
+# Print the mean accuracy and standar deviation over k folds
 cat("\nMean Accuracy over", k, "Folds:", mean_accuracy)
 cat("\nStandard Deviation of Accuracies over", k, "Folds:", sd_accuracy)
 
-
-# Get variable importance measures
+# Calculate and print the variable importance of the model
 var_imp <- randomForest::importance(rf_model)
-
-# Sort the variables by importance (in descending order)
 var_imp <- var_imp[order(var_imp[,4], decreasing = TRUE), ]
 var_imp <- as.data.frame(var_imp)
-
-# Print the variable importance table
 print(var_imp)
