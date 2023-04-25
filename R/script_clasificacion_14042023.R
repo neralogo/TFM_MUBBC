22f9d2115fcdadf2f5abae24d9bb05622adb2951
Nermina Logo Lendo <nerminalogolendo@gmail.com>
2023-04-25 20:47:47 +0200

    cv

--- R/script_clasificacion_14042023.R
+++ R/script_clasificacion_14042023.R
@@ -13,6 +13,8 @@ library(mltools)    # Provides a set of functions for machine learning tasks
 library(ggtext)     # Provides a set of functions for using formatted text in 
 #ggplot2 graphics
 library(pROC)       # Provides functions for analyzing and visualizing ROC curves
+library(plotly)
+
 
 # Sets the seed for reproducibility
 set.seed(677)
@@ -35,12 +37,12 @@ df <- df %>%
   mutate(Exome = recode_factor(Exome, "0" = "Negative", "1" = "Positive")) %>%
   mutate(Gender = recode_factor(Gender, "1" = "Male", "2" = "Female")) %>%
   mutate(Epilepsy = recode_factor(Epilepsy, "0" = "No", "1" = "Probably", 
-                                                          "2" = "Yes")) %>%
+                                  "2" = "Yes")) %>%
   mutate(Regression = recode_factor(Regression, "0" = "No", "1" = "Yes")) %>%
   mutate(Delivery_problems = recode_factor(Delivery_problems, "0" = "No", 
-                                                            "1" = "Yes")) %>%
+                                           "1" = "Yes")) %>%
   mutate(Age_symptoms = recode_factor(Age_symptoms, "0" = "<1", "1" = "1-3", 
-                                                              "2" = ">3"))
+                                      "2" = ">3"))
 
 # Remove rows with missing values in specific columns
 df_no_ID <- df[,c(2:10)]
@@ -64,7 +66,7 @@ mean(is.na(df_no_ID$Epilepsy)) * 100
 
 # Run multiple imputation on the data using the mice package
 mice_object <- mice(df_no_ID[,-9], m = 5, maxit = 50, method = c("", "", "",
-                                        "polyreg", "pmm", "logreg", "", ""))
+                                                                 "polyreg", "pmm", "logreg", "", ""))
 
 # Retrieve the imputed data from the mice object
 imputed_data <- complete(mice_object)
@@ -75,13 +77,24 @@ imputed_data <- cbind(imputed_data, df_no_ID$Exome)
 # Rename the Exome column to match the original dataset
 imputed_data <- dplyr::rename(imputed_data, "Exome" = "df_no_ID$Exome")
 
-# Create a histogram of the "Age_walking" column in the original data
-hist(df_no_ID$Age_walking, main = "Distribution before imputing", 
-     xlab = "Age_walking")
+# Calculate density distribution of the original and imputed data
+dens_orig <- density(df_no_ID$Age_walking, na.rm = TRUE)
+dens_imp <- density(imputed_data$Age_walking)
 
-#Create a histogram of the "Age_walking" column in the imputed data
-hist(imputed_data$Age_walking, main = "Distribution after imputing", 
-     xlab = "Age_walking")
+# Create data frames for the original and imputed data
+df_orig <- data.frame(x = dens_orig$x, y = dens_orig$y, type = "Original data")
+df_imp <- data.frame(x = dens_imp$x, y = dens_imp$y, type = "Imputed data")
+
+# Plot the density distributions using ggplot
+ggplot(data = data.frame(x = c(dens_orig$x, dens_imp$x), y = c(dens_orig$y, 
+                                                               dens_imp$y), type = rep(c("Original data", "Imputed data"), 
+                                                                                       each = length(dens_orig$x))), aes(x = x, y = y, color = type)) +
+  geom_line(size = 0.5) +
+  labs(x = "Age in months", y = "Density distribution", title = "Age walking") +
+  scale_color_manual(values = c("blue", "red")) +
+  theme(legend.position = "right") +
+  theme_minimal()
+
 
 
 ############################## DATA BALANCING ##################################
@@ -182,7 +195,6 @@ ggplot(evaluations_long, aes(x = Method, y = value, fill = variable)) +
 
 # Select the sampling method with the best performance
 if (best_method == "over") {
-  
   # Upsample the minority class using caret's upSample function
   balanced_df <- caret::upSample(x = imputed_data[, -9], y = imputed_data$Exome, 
                                  yname = "Exome")
@@ -205,15 +217,104 @@ trainIndex_RF <- createDataPartition(balanced_df$Exome, p = 0.7, list = FALSE)
 train_RF <- balanced_df[trainIndex_RF, ]
 test_RF <- balanced_df[-trainIndex_RF, ]
 
-# Set the number of folds for cross-validation
-k <- 10
+# Find the best metric value for the 
+bestmtry <- tuneRF(train_RF[,-9], train_RF[,9], mtryStart = 1, ntreeTry = 100, 
+                   improve = 0.05, stepFactor = 3, trace = T, plot = T, doBest = F)
 
-# Initialize an array to store the accuracy scores for each fold of cross-validation
-cv_accuracies <- rep(0, k)
+bestmtry <- which.max(bestmtry)
+
+# Set the number of folds for cross-validation
+k <- 5
 
 # Define the values of maxnodes and ntree to test
-maxnodes_values <- c(5, 7, 10)
+maxnodes_values <- c(3, 5, 7)
 ntree_values <- c(500, 1000, 1500)
+
+# Initialize matrix to store accuracies for each combination of hyperparameters
+accuracy_matrix <- matrix(nrow=length(maxnodes_values)*length(ntree_values), 
+                          ncol=3, 
+                          dimnames=list(NULL, c("Maxnodes", "NTree", "Accuracy")))
+
+# Loop over each combination of hyperparameters
+index <- 1
+for (maxnodes in maxnodes_values) {
+  for (ntree in ntree_values) {
+    
+    # Create k folds for cross-validation
+    folds <- createFolds(train_RF$Exome, k = k)
+    
+    # Initialize variable to keep track of the average accuracy across folds
+    avg_accuracy <- 0
+    
+    # Loop over each fold of cross-validation
+    for (i in 1:k) {
+      
+      # Split the data into training and validation sets for this fold
+      train_indices <- unlist(folds[-i])
+      valid_indices <- folds[[i]]
+      train_cv <- train_RF[train_indices, ]
+      valid_cv <- train_RF[valid_indices, ]
+      
+      # Train random forest model on the training set
+      rf_model <- randomForest(Exome ~ ., data = train_cv, importance = TRUE, 
+                               proximity = TRUE, ntree = ntree, maxnodes = maxnodes,
+                               mtry = bestmtry)
+      
+      # Make predictions on the validation set using the trained model
+      rf_pred <- predict(rf_model, newdata = valid_cv)
+      
+      # Calculate the confusion matrix and accuracy score for this fold
+      confusion_matrix <- table(valid_cv$Exome, rf_pred)
+      accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
+      
+      # Add the accuracy to the running total for this combination of hyperparameters
+      avg_accuracy <- avg_accuracy + accuracy
+      
+    }  # End of cross-validation loop
+    
+    # Calculate the average accuracy across all folds for this combination of hyperparameters
+    avg_accuracy <- avg_accuracy/k
+    
+    # Update the accuracy matrix with the hyperparameters and accuracy
+    accuracy_matrix[index, 1] <- maxnodes
+    accuracy_matrix[index, 2] <- ntree
+    accuracy_matrix[index, 3] <- avg_accuracy
+    
+    # Increment the index for the next combination of hyperparameters
+    index <- index + 1
+    
+  }  # End of ntree loop
+  
+}  # End of maxnodes loop
+
+
+# Find the row of the accuracy matrix with the highest accuracy
+best_row <- which.max(accuracy_matrix[, 3])
+
+# Extract the best hyperparameters from the accuracy matrix
+best_maxnodes <- accuracy_matrix[best_row, 1]
+best_ntree <- accuracy_matrix[best_row, 2]
+
+# Train random forest model on the full training set using the best hyperparameters
+rf_model <- randomForest(Exome ~ ., data = train_RF, importance = TRUE, 
+                         proximity = TRUE, ntree = best_ntree, maxnodes = best_maxnodes,
+                         mtry = bestmtry)
+
+# Make predictions on the test set using the trained model
+rf_pred <- predict(rf_model, newdata = test_RF)
+
+# Calculate the confusion matrix and accuracy of the predictions
+confusion_matrix <- table(test_RF$Exome, rf_pred)
+accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
+
+# Print the confusion matrix and accuracy on the test set, 
+# as well as the best hyperparameters used
+cat("\nBest Maxnodes:", best_maxnodes)
+cat("\nBest NTree:", best_ntree)
+cat("\nBest mtry:", bestmtry)
+cat("\nAccuracy on test set:", accuracy)
+cat("\n\nConfusion Matrix:")
+print(confusion_matrix)
 
 # Loop over each combination of hyperparameters
 for (maxnodes in maxnodes_values) {
@@ -233,7 +334,8 @@ for (maxnodes in maxnodes_values) {
       
       # Train random forest model on the training set
       rf_model <- randomForest(Exome ~ ., data = train_cv, importance = TRUE, 
-                               proximity = TRUE, ntree = ntree, maxnodes = maxnodes)
+                               proximity = TRUE, ntree = ntree, maxnodes = maxnodes,
+                               mtry = bestmtry)
       
       # Make predictions on the validation set using the trained model
       rf_pred <- predict(rf_model, newdata = valid_cv)
@@ -244,14 +346,15 @@ for (maxnodes in maxnodes_values) {
       cv_accuracies[i] <- accuracy
     }
     
-    # Calculate the mean and standard deviation of the accuracy scores over all folds 
-    # of cross-validation
+    # Calculate the mean and standard deviation of the accuracy scores over 
+    # all folds of cross-validation
     mean_accuracy <- mean(cv_accuracies)
     sd_accuracy <- sd(cv_accuracies)
     
     # Train a random forest model
     rf_model <- randomForest(Exome ~ ., data = train_RF, importance = TRUE, 
-                             proximity = TRUE, ntree = ntree, maxnodes = maxnodes)
+                             mtry = bestmtry, proximity = TRUE, ntree = ntree, 
+                             maxnodes = maxnodes)
     
     # Make predictions on the test set using the trained model
     rf_pred <- predict(rf_model, newdata = test_RF)
@@ -260,7 +363,8 @@ for (maxnodes in maxnodes_values) {
     confusion_matrix <- table(test_RF$Exome, rf_pred)
     accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
     
-    # Print the confusion matrix and accuracy on the test set, as well as the hyperparameters used
+    # Print the confusion matrix and accuracy on the test set, 
+    # as well as the hyperparameters used
     cat("\nMaxnodes:", maxnodes, "NTree:", ntree)
     cat("\nConfusion Matrix:\n")
     print(confusion_matrix)
@@ -273,24 +377,10 @@ for (maxnodes in maxnodes_values) {
 }
 
 
-# Train a random forest model
-rf_model <- randomForest(Exome ~ ., data = train_RF, importance = TRUE, 
-                         proximity = TRUE, ntree = 1000, mtry = 5, maxnodes = 7)
-
-# Make predictions on the test set using the trained model
-rf_pred <- predict(rf_model, newdata = test_RF)
-
-# Calculate the confusion matrix and accuracy of the predictions
-confusion_matrix <- table(test_RF$Exome, rf_pred)
-accuracy <- sum(diag(confusion_matrix))/sum(confusion_matrix)
-
-# Print the confusion matrix and accuracy on the test set
-cat("Confusion Matrix:\n")
-print(confusion_matrix)
-cat("\nAccuracy on Test Set:", accuracy)
-
 # Calculate and print the variable importance of the model
 var_imp <- randomForest::importance(rf_model)
 var_imp <- var_imp[order(var_imp[,4], decreasing = TRUE), ]
 var_imp <- as.data.frame(var_imp)
 print(var_imp)
+varImpPlot(rf_model)
+
